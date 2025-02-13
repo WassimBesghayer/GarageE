6ecc175695567f002d1006013c1d5c1c999893ad
Wassim <wassim.besghayer@gmail.com>
2025-02-13 15:06:40 +0100

    major step

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
 import { Routes, Route } from "react-router-dom";
 import "./App.css";
@@ -11,25 +11,37 @@ import { getreservation } from "./redux/reservationSlice";
 import NavBarr from "./components/NavBarr";
 import Home from "./components/Home";
 import Footer from "./components/Footer";
+import New_Home from "./components/New_Home";
+import BookReservation from "./components/Reservation";
+import Services from "./components/Services";
+import Shop from "./components/Shop";
+import Test from "./components/Test";
+
+
 
 function App() {
   const isAuth = localStorage.getItem("token");
   const dispatch = useDispatch();
+  const [ping, setping] = useState(false)
   useEffect(() => {
     dispatch(userCurrent());
     dispatch(getreservation());
-  });
+  },[ping])
   return (
     <div className="App">
       <NavBarr/>
       <Routes>
-        <Route exact path="/register" element={<Register />} />
+        <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
-        <Route element={<PrivateRoute />}>
-          <Route path="/profil" element={<Profil />} />
-        </Route>{" "}
+        {/* <Route element={<PrivateRoute />}> */}
+        <Route path="/profil" element={<Profil />} />
+        <Route path="/" element={<New_Home />} />
+        <Route path="/add" element={<BookReservation ping={ping} setping={setping} />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/shop" element={<Shop />} />
+        <Route path="/test" element={<Test />} />
       </Routes>
-      <Home/>
+
       <Footer/>
     </div>
   );
