9177b349beb73296989772b684c94d806ba59764
Wassim <wassim.besghayer@gmail.com>
2025-02-16 15:29:31 +0100

    initial commit

---
+++ client/src/components/Orders.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Orders() {
+  return (
+    <div>
+        Here are your orders :
+        {/* insert a 'ReactBootstrap table' */}
+    </div>
+  )
+}
+
+export default Orders
