141c62febe225f962cbba602d1fd9913bc96969f
Wassim <wassim.besghayer@gmail.com>
2025-02-19 19:21:08 +0100

    corrections

--- models/orders.js
+++ models/orders.js
@@ -1,16 +1,17 @@
 const mongoose = require("mongoose");
 const schema = mongoose.Schema; 
-const orderSchema = new schema({
-id_user:String,
-full_name_user:String,
-phone: String,
-email: String,
-items: String,
-unit_price: Number,
-total_price: Number,
-tax: Number,
-purchase_date:String,
-// status:{type:String, default:"preparing order"}
+const orderSchema = new schema ({
+
+    id_user:String,
+    full_name_user:String,
+    phone: String,
+    email: String,
+    items: String,
+    total_price: Number,
+    tax: Number,
+    purchase_date:String,
+    status:{type:String, default:" processing..."}
 });
-const Order=mongoose.model("Order", orderSchema);
-module.exports =Order
+
+const Order = mongoose.model("Order", orderSchema);
+module.exports = Order
