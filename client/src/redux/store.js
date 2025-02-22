679d598b80aeeb36d67b24609832f0e66c39272d
Wassim <wassim.besghayer@gmail.com>
2025-02-22 21:42:24 +0100

    articleSlice integrated

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,10 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit'
 import userSlice from './userSlice'
 import reservationSlice from './reservationSlice'
+import orderSlice from './orderSlice'
+import articleSlice from './articleSlice'
 
 export const store = configureStore({
   reducer: {
     user:userSlice,
-    reservation:reservationSlice
+    reservation:reservationSlice,
+    order:orderSlice,
+    article:articleSlice
   },
 })
