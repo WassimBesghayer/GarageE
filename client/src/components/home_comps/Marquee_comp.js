4819dfa90ba626a16f8876d027154a6adeabfb77
Wassim <wassim.besghayer@gmail.com>
2025-03-23 22:58:42 +0100

    fixing link pb

--- client/src/components/home_comps/Marquee_comp.js
+++ client/src/components/home_comps/Marquee_comp.js
@@ -4,7 +4,7 @@ import '../styling/marquee.css'
 function Marquee_comp() {
   return (
     <div className='marquee-section'>
-        <div id='title' style={{display: "flex", justifyContent: "center", fontSize:'40px'}}>Our partners</div>
+        <div id='title' style={{display: "flex", justifyContent: "center", fontSize:'40px'}}>Our Suppliers</div>
         <marquee direction="left" width="100%" height="150px" scrollamount='20' className='partners'>
             <img className='marquee-elem' src='https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fcdn.1min30.com%2Fwp-content%2Fuploads%2F2018%2F11%2FValeo-Logo.png&f=1&nofb=1&ipt=4d692c6d91b0bc0ac72ccca78c5382313ea3c7c8f1d4ac892f72dacaf2083669&ipo=images'/>
             <img className='marquee-elem' style={{width:"250px", height:"150px"}} src='https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.pinimg.com%2Foriginals%2F5b%2Fcb%2F02%2F5bcb022f0c354fdbb607c70f83a2de44.png&f=1&nofb=1&ipt=fd79c41283877eadb20982311a38361f65d40c64fe569825e99fb84729098f42&ipo=images'/>
@@ -15,7 +15,7 @@ function Marquee_comp() {
             <img className='marquee-elem' style={{width:'500px'}} src='https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Flogos-download.com%2Fwp-content%2Fuploads%2F2016%2F03%2FBosch-logo.png&f=1&nofb=1&ipt=cee1d288d841c8e260d55600a5a118261806021c42dde0d8da72dcbeef1b9c99&ipo=images'/>
             <img className='marquee-elem' style={{width:'500px', height:'200px'}} src='https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Flogos-world.net%2Fwp-content%2Fuploads%2F2020%2F09%2FMichelin-Symbol.png&f=1&nofb=1&ipt=20fad471eadf314b9d9f2eae0a8b0ab3a0cfb42668f60909e7e0553d21bba264&ipo=images'/>
             <img className='marquee-elem' style={{width:'500px', height:'80px'}} src='https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fmytommis.com%2Fwp-content%2Fuploads%2F2020%2F05%2FVarta-Logo.svg.png&f=1&nofb=1&ipt=2f3f3f5f86ba4286b2d4797a43d380b2f3d1f7b70df3e3bdfd921d9f1e7586a5&ipo=images'/>
-            <img className='marquee-elem' style={{width:'500px', height:'80px'}} src='https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.logolynx.com%2Fimages%2Flogolynx%2F5f%2F5fbadff249638afeedfae2de09fe572b.png&f=1&nofb=1&ipt=d6ed7a6af71c8d93719c375730a4efbe0ed5aaa18e11462ab690baebac95d780&ipo=images'/>
+            <img className='marquee-elem' style={{width:'500px', height:'80px'}} src='https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Flogonoid.com%2Fimages%2Fdenso-logo.png&f=1&nofb=1&ipt=ab893c78e1b61f0ca8920973f566c1365db3c9f02daa1aec217162c54b516f86&ipo=images'/>
             <img className='marquee-elem' style={{width:'220px', height:'180px'}} src='https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Flezebre.lu%2Fimages%2Fdetailed%2F17%2F30391-ZF.png&f=1&nofb=1&ipt=94c2b7986e34828bd5efc23b0c813fb9bfc1800dafb3a2e551afe904f1260c85&ipo=images'/>
         </marquee>
     </div>
