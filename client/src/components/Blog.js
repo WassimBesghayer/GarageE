3a8a8b3bd9fa86657f2558a4bda79e2a0d3f2b89
Wassim <wassim.besghayer@gmail.com>
2025-04-07 20:50:00 +0100

    fixing mapping pb

--- client/src/components/Blog.js
+++ client/src/components/Blog.js
@@ -1,9 +1,18 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
+import ArticleCard from '../components/blog/ArticleCard'
+
 
 function Blog() {
-  return (
-    <div>Welcome to our Blog</div>
-  )
+   const articles=useSelector((state)=>state.article.ArticleList)
+  
+   return (
+   <div>
+     <div className='blog-container'>
+        {articles.map((el)=><ArticleCard article={el}/>)}
+     </div>
+   </div>
+   )
 }
 
 export default Blog
