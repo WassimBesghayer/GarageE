cd755f3b624330ef8bc548e804b72eb47eaacb06
Wassim <wassim.besghayer@gmail.com>
2025-04-11 22:42:01 +0100

    init

---
+++ client/src/components/blog/ArticleModal.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+
+
+function ArticleModal() {
+    const [show, setShow] = useState(false);
+
+
+  return (
+    <div className='mymodal'> 
+
+
+ </div>
+  
+  )
+}
+
+export default ArticleModal
