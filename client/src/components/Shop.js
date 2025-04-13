35c547c659ee40f6017e24136b298a7362a7fd26
Wassim <wassim.besghayer@gmail.com>
2025-04-13 20:26:22 +0100

    v.1

--- client/src/components/Shop.js
+++ client/src/components/Shop.js
@@ -1,8 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
+import AddItem from './shop/AddItem'
+import ItemCard from './shop/ItemCard'
+import item from '../redux/itemSlice'
+
 
 function Shop() {
   return (
-    <div>Let's do some shopping</div>
+    <div>
+      <h2>Let's do some shopping: </h2>
+      <div> <ItemCard/> </div>
+      <div> <AddItem/> </div>
+      <div className='item-container'>
+        {item.map((el)=><ItemCard item={el}/>)}
+      </div>
+
+    </div>
   )
 }
 
