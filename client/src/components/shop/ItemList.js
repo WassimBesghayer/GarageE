3b4fffd2040bc4eeff116201071bdf0b302ad294
Wassim <wassim.besghayer@gmail.com>
2025-04-13 20:25:39 +0100

    v.1

--- client/src/components/shop/ItemList.js
+++ client/src/components/shop/ItemList.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import ItemCard from './ItemCard'
-import AddItem from './Ad'
+import AddItem from './AddItem'
 import { useSelector } from 'react-redux'
 
 function ItemList() {
@@ -10,7 +10,7 @@ function ItemList() {
   <div>
     <div><AddItem/></div>
     <div className='parent'>
-       {items.map((el)=><ItemCard item={el}/>)}
+      {items.map((el)=><ItemCard item={el}/>)}
     </div>
   </div>
   )
